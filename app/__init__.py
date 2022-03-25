6e8c9a0a45fdd30b242aa719afc3181bd0fe4eaf
Marco Visaya <marco.visaya527@gmail.com>
2022-03-25 22:19:22 +0800

    Add new cli commands for account creation

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 import os
+import click
 
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 from flask import Flask
+from flask.cli import AppGroup
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -19,6 +21,7 @@ app.config.update(
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+account_cli = AppGroup('account')
 
 from .api import api_admin, api_courier, api_orders, api_products, api_user
 from .database import tables
@@ -33,30 +36,38 @@ def make_shell_context():
 
 @app.cli.command("init-db")
 def init_db():
+    """ Initialize the database if not yet created """
     db.create_all()
     print("database created")
 
-
-@app.cli.command("destroy-db")
+@app.cli.command("clear-db")
 def destroy_db():
+    """ Clear all database data """
     db.drop_all()
-    print("database destroyed")
+    db.create_all()
+    print("database cleared")
 
-
-@app.cli.command("create-admin")
-def create_admin():
-    db.session.add(tables.Admin(username="admin",
+@account_cli.command('create')
+@click.argument('name')
+def create_user(name):
+    """ Create different user accounts """
+    if name == "user":
+        db.session.add(tables.User(email="user@gmail.com", password=os.environ.get('USER_PASSWORD'), phone="09312312313", firstname="Normal", lastname="User"))
+    elif name == "admin":
+        db.session.add(tables.Admin(username="admin",
                    password=os.environ.get('ADMIN_PASSWORD')))
+    elif name == "will_expire":
+        db.session.add(tables.User(email="will_expire@gmail.com", password=os.environ.get('USER_PASSWORD'), phone="09312312313", firstname="Will", lastname="Expire", password_expr=datetime.utcnow() + timedelta(days=8)))
+    elif name == "expired":
+        db.session.add(tables.User(email="expired@gmail.com", password=os.environ.get('USER_PASSWORD'), phone="09312312313", firstname="Expired", lastname="User", password_expr=datetime.utcnow()))
+    else:
+        return
     db.session.commit()
-    print("admin account created")
-
-@app.cli.command("create-user")
-def create_user():
-    user = tables.User(email="user@gmail.com", password=os.environ.get('USER_PASSWORD'), phone="09312312313", firstname="fname", lastname="lname")
-    db.session.add(user)
-    db.session.commit()
-    print("user account created")
+    print(f"{name} account created")
 
 @app.cli.command("view-config")
 def view_config():
+    """ View current app configurations """
     print(app.config)
+
+app.cli.add_command(account_cli)

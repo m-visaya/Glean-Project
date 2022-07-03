a75a20be95f8c60aaf7909ca9e42757c88655cf6
Marco Visaya <marco.visaya527@gmail.com>
2022-07-03 21:25:54 +0800

    Fix commit 55cc397

--- app/utils.py
+++ app/utils.py
@@ -149,8 +149,7 @@ def get_products(query=None, ordered=None):
     if ordered:
         return db.session.query(Product).order_by(Product.stock)
     if query is not None:
-        return db.session.query(Product).filter(
-            Product.name.contains(query)).all()
+        return db.session.query(Product).filter(Product.name.contains(query.lower())).all()
 
     return db.session.query(Product).all()
 
